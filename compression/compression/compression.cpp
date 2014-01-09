9d11ea1565a7d302f871f6b4298bf85b4c809c66
lisicky <lisicky@inbox.ru>
2014-01-09 17:53:18 +0400

    added alignment for elements of dictionary

--- compression/compression/compression.cpp
+++ compression/compression/compression.cpp
@@ -4,6 +4,20 @@
 
 int main(int argc, char* argv[])
 {
+		FILE *in,*out;
+	returnCode code;
+	in = fopen("C:\\Users\\God\\Desktop\\yyy.bmp","rb");
+	out =fopen("C:\\Users\\God\\Desktop\\yyy.lz78","wb");
+	code = lz78_encode(in,out);
+	printf("%d",code);
+	fclose(in);
+	fclose(out); 
+	in = fopen("C:\\Users\\God\\Desktop\\yyy.lz78","rb");
+	out = fopen("C:\\Users\\God\\Desktop\\yyy1.bmp","wb");
+	code = lz78_decode(in,out);
+	printf("%d",code);
+	fclose(in);
+	fclose(out); 
 	return 0;
 }
 
--- compression/compression/lz.h
+++ compression/compression/lz.h
@@ -7,12 +7,13 @@ typedef enum returnCode
 	write_error,
 	not_enough_memory
 }returnCode;
-
+#pragma pack(push,1)
 typedef struct dictionaryElem
 {
 	char elem;
 	unsigned short n;
 }dictionaryElem;
+#pragma pack(pop)
 
 returnCode lz78_encode(FILE* in, FILE* out);
 returnCode lz78_decode(FILE* in, FILE* out);
